b5717691f75bd9b03698b3768803b2aac0865aeb
caseyfee <caseyfee3@gmail.com>
2023-03-21 17:11:59 -0700

    drafted the rest of the functions

--- db.js
+++ db.js
@@ -17,10 +17,39 @@ const db = mysql.createConnection(
     console.log(`Connected to the ArtHouse_db database.`)
 );
 
+
 const queries = {
     getAllDept: async function () {
         try {
             const [rows] = await db.promise().query("SELECT * FROM department");
+            return rows
+        } catch (err) {
+            console.error(err)
+        }
+    },
+
+    getAllRoles: async function () {
+        try {
+            const [rows] = await db.promise().query("SELECT * FROM roles");
+            return rows
+        } catch (err) {
+            console.error(err)
+        }
+    }, 
+
+    getAllEmployees: async function () {
+        try {
+            const [rows] = await db.promise().query("SELECT * FROM employees");
+            return rows
+        } catch (err) {
+            console.error(err)
+        }
+    },
+
+    updateEmployees: async function () {
+        try {
+            // is promise().post a real thing?
+            const [rows] = await db.promise().post("SELECT * FROM employees");
             return rows
         } catch (err) {
             console.error(err)
